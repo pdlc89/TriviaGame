dd15f5756825445de68810d313f60eb47a01e965
Pedro <pcruz9332@gmail.com>
2017-11-04 01:40:06 -0700

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,88 @@
 $(document).ready(function(){
 
-// create global variables
-var count = 10;
+//variables
+var count = 60;
 var computerGuess;
 var userGuess;
 var questionGuess;
-var gamesPlayed;
-var gamesWon;
-var gamesLost;
+var gamesPlayed= 0;
+var gamesWon = 0;
+var gamesLost = 0;
 var questionArray = [];
+var ifWon = false;
+var isGameStarted = false;
 var topics = ["BIOLOGY", "MATH", "MOVIES", "COMPUTERS"];
-// functions
-function newGame() {
-	$("#question-display, #answers").empty();
-	var reStart = $("<button>");
-	reStart.attr("id", "start-button")
-	reStart.text("Start");
-	$("#question-display").append(reStart);
-}
-
-function gameRestart() {
-	gamesWon++;
-	$("#answers").empty();
-	$("#question-display").text("Correct!")
-	setTimeout(newGame, 500)
-}
-
-
-
 //Array of possible answers
 var math = ["1", "pi", "0", "root(-1)", "2.667561", "infinity"]
 var bio = ["Ribosomes", "Mitocondria", "R.N.A."];
 var mvs = ["Jurasic Park", "The Lost World", "III", "Jurasic World"];
 var cs = [";", ":", "-", "}"];
 
-// Starts the game
-	$("#start-button").on("click", function(){
-	var timer = setInterval(function() {
-  			count=count-1;
-  				if (count <= 0) {
-     				clearInterval(counter);
-     			gamesLost++;
-     			newGame();
-  				}
- 			$("#timer-display").html("You have " + count + " secs left");
-			}, 1000);
+// functions
+function newGame() {
+	$("#answers, #timer-display, #question-display").empty();
+	if (count != 0){
+		count = 60;
+		isGameStarted = false;
+	}
+}
 
+function gameRestart() {
+	gamesWon++;
+	$("#answers").empty();
+	$("#question-display").text("Correct!")
+  		$( "#question-display" ).animate({
+   				 width: "600px",
+   				 opacity: 0.5,
+   				 marginLeft: "0.6in",
+   				 fontSize: "3em",
+   				 borderWidth: "10px"
+  				}, 800, function(){$(this).removeAttr("style")});
+	  			setTimeout(1000);
+	$("#wins").text(gamesWon);
+}
+
+function gameStart () {
 	//1. start the game and erase the "Start" button
-	$("#question-display").empty();
+	$("#question-display, #answers").empty();
 		gamesPlayed++;
 	// Create an array with the list of the question topics along with an array of guesses for each topic
 	//an arry within an array if you will.
 		computerGuess = topics[Math.floor(Math.random()*topics.length)];
-		console.log (computerGuess);
+		console.log ("The Topic is: " + computerGuess);
+		$("#games-played").text(gamesPlayed);
 
 	//2. Place the question in the the id "question-display"
 		if (computerGuess === "MATH") {
 			questionGuess = "e^(i*pi) + 1 = ?";
 			questionArray = math;
-			//$("#").css("background-image", "url(/assets/images/Euler.jpg")
+			$("#question-display").css("background-image", "url('C://users/pedro/code/triviagame/assets/images/euler.png')");
 		}
 		else if (computerGuess === "BIOLOGY") {
 			questionGuess = "What is inside the nucleus of a cell, other than D.N.A.?";
 			questionArray = bio;
-			//$("#").css("background-image", "url(/assets/images/Euler.jpg")
+			$("#question-display").css("background-image", "url('C://users/pedro/code/triviagame/assets/images/cell.png')");
 		}
 		else if(computerGuess === "MOVIES") {
 			questionGuess = "What movie does Jurasic Park Movie does Vince Von come out in?";
 			questionArray = mvs;
-			//$("#").css("background-image", "url(/assets/images/Euler.jpg")
+			$("#question-display").css("background-image", "url('C://users/pedro/code/triviagame/assets/images/jp.png')");
 		}
 		else {
 			questionGuess = "What is the symbol to end a line of code in most Computer Languages?"
 			questionArray = cs;
-			//$("#").css("background-image", "url(/assets/images/Euler.jpg")
+			$("#question-display").css("background-image", "url('C://users/pedro/code/triviagame/assets/images/Cs.png')");
 		}
 	$("#question-display").html("<h1>" + questionGuess + "</h1>");
-	//3. Create a for loop that will create buttons from the answers array
+	//Create a for loop that will create buttons from the answers array
         for (var i = 0; i < questionArray.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var btn = $("<button>");
-          // Adding a class
           btn.addClass("answer-button");
-          // Added a data-attribute
           btn.attr("value", questionArray[i]);
-          // Provided the initial button text
           btn.text(questionArray[i]);
-          // Added the button to the HTML
           $("#answers").append(btn);
-
         }
-	//4. Create a timer and display the timer at the id "timer-display"
+	//Checks if the user chose the correct answer if not puts up alert
 	$(".answer-button").on("click", function() {
 		userGuess = this.value;
 		console.log("The Users Guess is: " + userGuess);
@@ -110,21 +100,36 @@ var cs = [";", ":", "-", "}"];
 		}else {
 			alert("Plese try again.");
 			gamesLost++;
+			$("#loses").text(gamesLost);
 			return;
 		}
 	});
+	if (count != 0) {
+		setTimeout(gameStart, 10000);
+	}else {
+		newGame();
+	}
+}
 
-	setTimeout(newGame, 10000);
+// Starts the game
+	$("#start-button").on("click", function(){
+		if (isGameStarted === false) {
+			gameStart();
+			isGameStarted = true;
+
+			//Starts the countdown
+			if (isGameStarted) {
+			var timer = setInterval(function() {
+		  			count=count-1;
+		  				if (count <= 0) {
+		     				clearInterval(timer);
+		     			isGameStarted = false;
+		     			newGame();
+		  				}
+		 			$("#timer-display").html("You have " + count + " secs left");
+					}, 1000);
+			}
+		}
 	});
-	//5. Create an event handler that will allow the user to choose an answer
-	//6. In the event handler check if the user has chosen the correct answer
-
-
-
-
-/* clear the "display-question" and "answers" divs and place "Your Score so far:" 
-	in the the "display-question" along with the tally. 
-*/
-
-
+	
 });
